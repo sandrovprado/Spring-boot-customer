9899e4366dc7d5f1f31558010602613b40ecf783
Sandro <sandrovprado@gmail.com>
2023-07-13 22:42:53 -0400

    Render customers/loading spinner/Card component/Wrap item

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,22 +1,62 @@
-import { Button, ButtonGroup } from '@chakra-ui/react';
-import SidebarWithHeader from "./shared/SideBar.jsx";
-import { useEffect } from "react";
+import {Wrap, WrapItem, Spinner, Text, Card} from '@chakra-ui/react';
+import SidebarWithHeader from "./components/shared/SideBar.jsx";
+import {useEffect, useState} from "react";
 import {getCustomers} from "./services/client.js";
+import CardWithImage from "./components/Card.jsx";
 
 
 const App = () => {
 
-    useEffect(() =>{
-        getCustomers().then(res =>{
-            console.log(res)
-        }).catch(err =>{
-            console.log(err)
-        })
-    },[])
+    const [customers, setCustomers] = useState([]);
+    const [loading, setLoading] = useState(false);
 
-    return(
+    useEffect(() => {
+        setLoading(true)
+        getCustomers().then(res => {
+            setCustomers(res.data)
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        }).finally(() => {
+            setLoading(false)
+        })
+    }, [])
+
+    if (loading) {
+        return (<SidebarWithHeader>
+                <Spinner
+                    thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl'
+                />
+            </SidebarWithHeader>
+        )
+    }
+
+    if (customers.length <= 0) {
+        return (
+            <SidebarWithHeader>
+                <Text>No customers available</Text>
+            </SidebarWithHeader>
+        )
+    }
+
+    return (
         <SidebarWithHeader>
-            <Button colorScheme='teal' variant='outline'>Click me</Button>
+            <Wrap spacing={"30px"} justify={"center"}>
+                {customers.map((customer, index) => (
+                    <WrapItem key={index}>
+                        <CardWithImage
+                            {...customer}
+                            // name={customer.name}
+                            // age={customer.age}
+                            // email={customer.email}
+                        />
+                    </WrapItem>
+                ))}
+            </Wrap>
         </SidebarWithHeader>
     )
 }
